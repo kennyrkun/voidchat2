5e2c6997f9e019fcaa9c88a50fa99a97133ec348
kennyrkun <kennyrmccormick+github@gmail.com>
2019-12-02 13:49:59 -0600

    update Message class
    add packet overloads so that we can easily send them.
    also allow them to be created on the fly and then zipped up into a packet and sent off

--- voidchat2/Message.cpp
+++ voidchat2/Message.cpp
@@ -1,1 +1,25 @@
 #include "Message.hpp"
+
+#include <SFML/Network.hpp>
+
+sf::Packet& operator <<(sf::Packet& packet, const Message& message)
+{
+	packet << "outgoingMessage";
+	packet << message.author;
+	packet << message.messageContent;
+	return packet;
+}
+
+sf::Packet& operator >>(sf::Packet& packet, Message& message)
+{
+	// assume that the command object has already been extracted
+	packet >> message.author;
+	packet >> message.messageContent;
+	return packet;
+}
+
+Message::Message(const std::string& author, const std::string& message)
+{
+	this->author = author;
+	this->messageContent = message;
+}
--- voidchat2/Message.hpp
+++ voidchat2/Message.hpp
@@ -1,14 +1,22 @@
 #ifndef MESSAGE_HPP
 #define MESSAGE_HPP
 
+#include <SFML/Network.hpp>
+
 #include <string>
 
 class Message
 {
 public:
-	std::string author;
+	Message(const std::string& author, const std::string& message);
 
+	std::string author;
 	std::string messageContent;
+
+	// TODO: time sent
 };
+
+sf::Packet& operator <<(sf::Packet& packet, const Message& command);
+sf::Packet& operator >>(sf::Packet& packet, Message& command);
 
 #endif // !MESSAGE_HPP
