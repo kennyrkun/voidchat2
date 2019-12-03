67dc5f001c7025772bb49518016040e1b56cb93c
Kenny McCormick <keennymccormic+github@gmail.com>
2019-12-02 23:01:19 -0600

    move you were poked into audio folder

--- voidchat2/VoidChat.cpp
+++ voidchat2/VoidChat.cpp
@@ -24,7 +24,7 @@ VoidChat::VoidChat()
 	{
 		{
 			std::cout << "sounds" << std::endl;
-			notificationSoundBuffer.loadFromFile("./resource/you_were_poked.wav");
+			notificationSoundBuffer.loadFromFile("./resource/audio/you_were_poked.wav");
 			notificationSound.setBuffer(notificationSoundBuffer);
 		}
 
